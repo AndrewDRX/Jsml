82ae7e74f14915d0a53995ecba5bb359fa0d28ba
Andrew <andrewdrx@live.com>
2020-03-08 16:51:59 -0400

    WebSocket for server.

--- Server/JsmlWebServer.cs
+++ Server/JsmlWebServer.cs
@@ -1,10 +1,16 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Threading;
+
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
-using Jsml.Enums;
+using System.Net.WebSockets;
+
 using Jsml.Loadables;
 
 namespace Jsml.Server
@@ -12,14 +18,48 @@ namespace Jsml.Server
     class JsmlWebServer
     {
         public static IWebHost Create(LoadableElement root) =>
-            WebHost
-                .CreateDefaultBuilder()
-                .Configure(configureApp: app =>
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .Configure(configureApp: app => {
+                    app.UseWebSockets();
+                    app.Use(async (context, next) => {
+                        if (context.Request.Path == "/ws")
+                        {
+                            if (context.WebSockets.IsWebSocketRequest)
+                            {
+                                WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                                var buffer = new byte[1024 * 4];
+                                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                                while (!result.CloseStatus.HasValue)
+                                {
+                                    await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType,
+                                    result.EndOfMessage, CancellationToken.None);
+                                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                                }
+                                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                            }
+                            else
+                            {
+                                context.Response.StatusCode = 400;
+                            }
+                        }
+                        else
+                        {
+                            await next();
+                        }
+                    });
+                    app.UseFileServer();
+                    /*
                     app.Run(handler: async context => {
                         context.Response.ContentType = "text/html";
-                        await context.Response.WriteAsync(text: root.Dom.ToString());
-                    })
-                )
+                        await context.Response.WriteAsync(
+                            text: root.Dom.ToString()
+                        );
+                    });
+                    */
+                })
                 .Build();
     }
 }
